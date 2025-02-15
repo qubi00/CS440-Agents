8489f23afdfa4f209c0caeb052be33a3beaa0d13
Qubi <jason1004qiu@gmail.com>
2025-02-15 18:31:36 -0500

    finished bfs and dfs

--- src/labs/stealth/agents/BFSMazeAgent.java
+++ src/labs/stealth/agents/BFSMazeAgent.java
@@ -7,7 +7,7 @@ import edu.bu.labs.stealth.graph.Path;
 
 
 import edu.cwru.sepia.environment.model.state.State.StateView;
-
+import edu.cwru.sepia.util.Direction;
 
 import java.util.HashSet;       // will need for bfs
 import java.util.Queue;         // will need for bfs
@@ -31,7 +31,62 @@ public class BFSMazeAgent
     public Path search(Vertex src,
                        Vertex goal,
                        StateView state)
-    {
+    {   
+
+        Queue<Path> q = new LinkedList<>();
+        Set<Vertex> visited = new HashSet<>();
+
+        q.add(new Path(src, 1.0f, null));
+        visited.add(src);
+
+        Direction[] directions = {Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST,
+            Direction.NORTHEAST, Direction.NORTHWEST, Direction.SOUTHEAST, Direction.SOUTHWEST};
+        
+        while(!q.isEmpty()){
+            Path currPath = q.poll();
+            Vertex currVertex = currPath.getDestination();
+            int currRow = currVertex.getXCoordinate();
+            int currCol = currVertex.getYCoordinate();
+
+            if(currVertex.equals(goal)){
+                System.out.println(currPath.toString());
+                return currPath;
+            }
+
+            
+            for(int i = 0; i < directions.length; i++){
+                Direction dir = directions[i];
+                int newRow = currRow;
+                int newCol = currCol;
+                if (dir == Direction.NORTH) {
+                    newRow -= 1;
+                } else if (dir == Direction.SOUTH) {
+                    newRow += 1;
+                } else if (dir == Direction.WEST) {
+                    newCol -= 1;
+                } else if (dir == Direction.EAST) {
+                    newCol += 1;
+                } else if (dir == Direction.NORTHEAST) {
+                    newRow -= 1;
+                    newCol += 1;
+                } else if (dir == Direction.NORTHWEST) {
+                    newRow -= 1;
+                    newCol -= 1;
+                } else if (dir == Direction.SOUTHEAST) {
+                    newRow += 1;
+                    newCol += 1;
+                } else if (dir == Direction.SOUTHWEST) {
+                    newRow += 1;
+                    newCol -= 1;
+                }
+                if(state.inBounds(newRow, newCol) && !state.isResourceAt(newRow, newCol) && !visited.contains(new Vertex(newRow, newCol))){
+                    Vertex neighbor = new Vertex(newRow, newCol);
+                    visited.add(neighbor);
+                    Path newPath = new Path(neighbor, 1.0f, currPath);
+                    q.add(newPath);
+                }
+            }
+        }
         return null;
     }
 
--- src/labs/stealth/agents/DFSMazeAgent.java
+++ src/labs/stealth/agents/DFSMazeAgent.java
@@ -7,9 +7,11 @@ import edu.bu.labs.stealth.graph.Path;
 
 
 import edu.cwru.sepia.environment.model.state.State.StateView;
-
+import edu.cwru.sepia.util.Direction;
 
 import java.util.HashSet;   // will need for dfs
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;     // will need for dfs
 import java.util.Set;       // will need for dfs
 
@@ -30,7 +32,62 @@ public class DFSMazeAgent
     public Path search(Vertex src,
                        Vertex goal,
                        StateView state)
-    {
+    {   
+
+        Stack<Path> q = new Stack<>();
+        Set<Vertex> visited = new HashSet<>();
+
+        q.add(new Path(src, 1.0f, null));
+        visited.add(src);
+
+        Direction[] directions = {Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST,
+            Direction.NORTHEAST, Direction.NORTHWEST, Direction.SOUTHEAST, Direction.SOUTHWEST};
+        
+        while(!q.isEmpty()){
+            Path currPath = q.pop();
+            Vertex currVertex = currPath.getDestination();
+            int currRow = currVertex.getXCoordinate();
+            int currCol = currVertex.getYCoordinate();
+
+            if(currVertex.equals(goal)){
+                System.out.println(currPath.toString());
+                return currPath;
+            }
+
+            
+            for(int i = 0; i < directions.length; i++){
+                Direction dir = directions[i];
+                int newRow = currRow;
+                int newCol = currCol;
+                if (dir == Direction.NORTH) {
+                    newRow -= 1;
+                } else if (dir == Direction.SOUTH) {
+                    newRow += 1;
+                } else if (dir == Direction.WEST) {
+                    newCol -= 1;
+                } else if (dir == Direction.EAST) {
+                    newCol += 1;
+                } else if (dir == Direction.NORTHEAST) {
+                    newRow -= 1;
+                    newCol += 1;
+                } else if (dir == Direction.NORTHWEST) {
+                    newRow -= 1;
+                    newCol -= 1;
+                } else if (dir == Direction.SOUTHEAST) {
+                    newRow += 1;
+                    newCol += 1;
+                } else if (dir == Direction.SOUTHWEST) {
+                    newRow += 1;
+                    newCol -= 1;
+                }
+                if(state.inBounds(newRow, newCol) && !state.isResourceAt(newRow, newCol) && !visited.contains(new Vertex(newRow, newCol))){
+                    Vertex neighbor = new Vertex(newRow, newCol);
+                    visited.add(neighbor);
+                    Path newPath = new Path(neighbor, 1.0f, currPath);
+                    q.add(newPath);
+                }
+            }
+        }
         return null;
     }
 
