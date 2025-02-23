15fe0135f86206b649eaaf323bd92b0cc0403897
Qubi <jason1004qiu@gmail.com>
2025-02-23 16:11:05 -0500

    updated with pa1

---
+++ src/pas/stealth/agents/StealthAgent.java
@@ -0,0 +1,158 @@
+package src.pas.stealth.agents;
+
+
+// SYSTEM IMPORTS
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.environment.model.history.History.HistoryView;
+import edu.cwru.sepia.environment.model.state.State.StateView;
+import edu.cwru.sepia.environment.model.state.Unit.UnitView;
+import edu.cwru.sepia.util.Direction;
+
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+// JAVA PROJECT IMPORTS
+import edu.bu.pas.stealth.agents.AStarAgent;                // the base class of your class
+import edu.bu.pas.stealth.agents.AStarAgent.AgentPhase;     // INFILTRATE/EXFILTRATE enums for your state machine
+import edu.bu.pas.stealth.agents.AStarAgent.ExtraParams;    // base class for creating your own params objects
+import edu.bu.pas.stealth.graph.Vertex;                     // Vertex = coordinate
+import edu.bu.pas.stealth.graph.Path;                       // see the documentation...a Path is a linked list
+
+
+
+public class StealthAgent
+    extends AStarAgent
+{
+
+    // Fields of this class
+    // TODO: add your fields here! For instance, it might be a good idea to
+    // know when you've killed the enemy townhall so you know when to escape!
+    // TODO: implement the state machine for following a path once we calculate it
+    //       this will for sure adding your own fields.
+    private int enemyChebyshevSightLimit;
+    
+
+    public StealthAgent(int playerNum)
+    {
+        super(playerNum);
+
+        this.enemyChebyshevSightLimit = -1; // invalid value....we won't know this until initialStep()
+    }
+
+    // TODO: add some getter methods for your fields! Thats the java way to do things!
+    public final int getEnemyChebyshevSightLimit() { return this.enemyChebyshevSightLimit; }
+
+    public void setEnemyChebyshevSightLimit(int i) { this.enemyChebyshevSightLimit = i; }
+
+
+    ///////////////////////////////////////// Sepia methods to override ///////////////////////////////////
+
+    /**
+        TODO: if you add any fields to this class it might be a good idea to initialize them here
+              if they need sepia information!
+     */
+    @Override
+    public Map<Integer, Action> initialStep(StateView state,
+                                            HistoryView history)
+    {
+        super.initialStep(state, history); // call AStarAgent's initialStep() to set helpful fields and stuff
+
+        // now some fields are set for us b/c we called AStarAgent's initialStep()
+        // let's calculate how far away enemy units can see us...this will be the same for all units (except the base)
+        // which doesn't have a sight limit (nor does it care about seeing you)
+        // iterate over the "other" (i.e. not the base) enemy units until we get a UnitView that is not null
+        UnitView otherEnemyUnitView = null;
+        Iterator<Integer> otherEnemyUnitIDsIt = this.getOtherEnemyUnitIDs().iterator();
+        while(otherEnemyUnitIDsIt.hasNext() && otherEnemyUnitView == null)
+        {
+            otherEnemyUnitView = state.getUnit(otherEnemyUnitIDsIt.next());
+        }
+
+        if(otherEnemyUnitView == null)
+        {
+            System.err.println("[ERROR] StealthAgent.initialStep: could not find a non-null 'other' enemy UnitView??");
+            System.exit(-1);
+        }
+
+        // lookup an attribute from the unit's "template" (which you can find in the map .xml files)
+        // When I specify the unit's (i.e. "footman"'s) xml template, I will use the "range" attribute
+        // as the enemy sight limit
+        this.setEnemyChebyshevSightLimit(otherEnemyUnitView.getTemplateView().getRange());
+
+        return null;
+    }
+
+    /**
+        TODO: implement me! This is the method that will be called every turn of the game.
+              This method is responsible for assigning actions to all units that you control
+              (which should only be a single footman in this game)
+     */
+    @Override
+    public Map<Integer, Action> middleStep(StateView state,
+                                           HistoryView history)
+    {
+        Map<Integer, Action> actions = new HashMap<Integer, Action>();
+
+        /**
+            I would suggest implementing a state machine here to calculate a path when neccessary.
+            For instance beginning with something like:
+
+            if(this.shouldReplacePlan(state))
+            {
+                // recalculate the plan
+            }
+
+            then after this, worry about how you will follow this path by submitting sepia actions
+            the trouble is that we don't want to move on from a point on the path until we reach it
+            so be sure to take that into account in your design
+
+            once you have this working I would worry about trying to detect when you kill the townhall
+            so that you implement escaping
+         */
+
+        return actions;
+    }
+
+    ////////////////////////////////// End of Sepia methods to override //////////////////////////////////
+
+    /////////////////////////////////// AStarAgent methods to override ///////////////////////////////////
+
+    public Collection<Vertex> getNeighbors(Vertex v,
+                                           StateView state,
+                                           ExtraParams extraParams)
+    {
+        return null;
+    }
+
+    public Path aStarSearch(Vertex src,
+                            Vertex dst,
+                            StateView state,
+                            ExtraParams extraParams)
+    {
+        return null;
+    }
+
+    public float getEdgeWeight(Vertex src,
+                               Vertex dst,
+                               StateView state,
+                               ExtraParams extraParams)
+    {
+        return 1f;
+    }
+
+    public boolean shouldReplacePlan(StateView state,
+                                     ExtraParams extraParams)
+    {
+        return false;
+    }
+
+    //////////////////////////////// End of AStarAgent methods to override ///////////////////////////////
+
+}
+
